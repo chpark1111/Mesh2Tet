abdfaff0ae5106671a5e8b40f23d8de2dd28d088
Chanhyeok <chpark1111@kaist.ac.kr>
2022-12-23 14:27:07 +0900

    delete user stuff

--- scripts/make_dataset/run_all2.sh
+++
@@ -1,2 +0,0 @@
-python3 mesh2tetra.py --e 5e-3 --l 0.8 --num_worker 80
-python3 mesh2tetra.py --data_path /home/chpark1111/docker/geometry2/research/shapenet/shapenet_chair --result_path /home/chpark1111/docker/geometry2/research/Mesh2Tet/result/shapenet_chair --num_worker 80 --e 5e-3 --l 0.8
